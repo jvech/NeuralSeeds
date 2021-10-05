7dda89973f15a3edbfcc228e847e56a8811b35b9
jvech <jmvalenciae@unal.edu.co>
2021-10-05 15:52:53 -0500

    Finally Anchor Box Concept Clarified
    Now i have to do the code to encode the labels to anchor boxes

--- src/utils/data.py
+++ src/utils/data.py
@@ -3,94 +3,108 @@ from os import path
 import cv2
 import tensorflow as tf
 import numpy as np
+import xml.etree.ElementTree as ET
 
-def mask2categorical(mask: "tf.Tensor", labels: dict) -> "tf.Tensor":
-    """Pass a certain rgb mask (3-channels) to an image of ordinal classes"""
+def get_xyhw(box_corners):
+    xmin = box_corners[:, 0] 
+    ymin = box_corners[:, 1] 
+    xmax = box_corners[:, 2] 
+    ymax = box_corners[:, 3] 
+    classes = box_corners[:, -1]
 
-    assert type(labels) == dict, "labels variable should be a dictionary"
-    X = mask
+    w = xmax - xmin
+    h = ymax - ymin
 
-    if X.dtype == "float32":
-        X = tf.cast(X * 255, dtype="uint8")
+    x = xmin + w/2
+    y = ymin + h/2
+    return tf.stack([x, y, w, h, classes], axis=1)
 
-    Y = tf.zeros(X.shape[0:2] , dtype="int32")
-    for i, key in enumerate(labels):
-        Y = tf.where(tf.reduce_all(X == tf.constant(labels[key], "uint8"), axis=-1), i, Y)
-    Y = tf.cast(Y, dtype=tf.uint8)
-    return tf.expand_dims(Y, axis=-1)
+def get_corners(box_xyhw):
+    x = box_xyhw[:, 0]
+    y = box_xyhw[:, 1]
+    w = box_xyhw[:, 2]
+    h = box_xyhw[:, 3]
+    classes = box_xyhw[:, -1]
 
-def categorical2mask(categorical: "tf.Tensor", labels: dict) -> "tf.Tensor":
-    X = categorical
-    Y = np.zeros(X.shape[0:2] + [3], dtype="uint8")
-    for i, key in enumerate(labels):
-        Y[...,0] = np.where(X==i, labels[key][0], Y[...,0])
-        Y[...,1] = np.where(X==i, labels[key][1], Y[...,1])
-        Y[...,2] = np.where(X==i, labels[key][2], Y[...,2])
-    return Y
+    xmin = x - w/2
+    xmax = x + w/2
+    ymin = y - h/2
+    ymax = y + h/2
 
-def parse_labelfile(path):
-    """Return a dict with the corresponding rgb mask values of the labels
-        Example:
-        >>> labels = parse_labelfile("file/path")
-        >>> print(labels) 
-        >>> {"label1": (r1, g1, b1), "label2": (r2, g2, b2)} 
-    """
-    with open(path, "r") as FILE:
-        lines = FILE.readlines()
+    return tf.stack([xmin, ymin, xmax, ymax, classes], axis=1)
 
-    labels = {x.split(":")[0]: x.split(":")[1] for x in lines[1:]}
+def bndboxes_draw(img: "tf.Tensor", boxes: "tf.Tensor") -> "np.ndarray":
+    X = img.numpy().astype("uint8").copy()
+    Y = boxes.numpy().astype("uint").copy()
+    for x1, y1, x2, y2, c in Y:
+        pt1, pt2 = (x1, y1), (x2, y2)
+        if c == 1: COLOR = (255, 0, 0)
+        elif c == 2: COLOR = (255, 255, 0)
+        else: COLOR = (0, 0, 0)
+        X = cv2.rectangle(X, pt1, pt2, COLOR, 2)
+    return X
 
-    for key in labels:
-        labels[key] = np.array(labels[key].split(",")).astype("uint8")
+def data_read(imgs_path: str, anns_path: str, img_size: '(int, int)' = (624, 624)) -> 'tf.data.Dataset':
+    anns = [path.join(anns_path, ann_file) for ann_file in os.listdir(anns_path)]
+    imgs_folder = imgs_path
 
-    return labels
+    def parse_xml(root):
+        x1y1_x2y2_c = []
+        for member in root.findall('object'):
+            mclass = (1.0 if 'im' in member[0].text else 
+                     (2.0 if 'el' in member[0].text else 0)) 
+            xmin, ymin, xmax, ymax = member.find("bndbox")
+            x1y1 = [float(xmin.text) , float(ymin.text) ]
+            x2y2 = [float(xmax.text) , float(ymax.text) ]
+            x1y1_x2y2_c.append(x1y1 + x2y2 + [mclass])
+        return x1y1_x2y2_c 
 
-## Dataset Functions
-def read_data(img_paths, label_paths):
-    def imgs_read():
-        for X, Y in zip(img_paths, label_paths):
-            yield (tf.constant(cv2.imread(X)[..., ::-1], tf.uint8),
-                   tf.constant(cv2.imread(Y)[..., ::-1], tf.uint8))
-    IMG_DIMS = cv2.imread(img_paths[0]).shape
+    def imglab_read():
+        for ann in anns:
+            root = ET.parse(ann).getroot()
+
+            img_name = root.find('filename').text
+            img_name = path.join(imgs_folder, img_name)
+            img = cv2.cvtColor(cv2.imread(img_name), cv2.COLOR_BGR2RGB) 
+            boxes = parse_xml(root)
+            yield tf.constant(img, "float32"), tf.constant(boxes, "float32")
+
+    width, height = img_size
     ds = tf.data.Dataset.from_generator(
-            imgs_read,
-            output_signature= (tf.TensorSpec(IMG_DIMS, tf.uint8),
-                               tf.TensorSpec(IMG_DIMS, tf.uint8))
+            imglab_read,
+            output_signature = (tf.TensorSpec((width, height, 3), "float32"),
+                                tf.TensorSpec((None, 5), "float32")) 
             )
     return ds
 
-def preprocess_ds(ds: "tf.data.Dataset", labelmap: dict, shape: tuple) ->  "tf.data.Dataset":
-    def normalize(img, label):
-        return tf.cast(img, tf.float32) / 255., label
+def data_preprocess(ds, new_size=(300, 300)):
+    def img_pre(img, boxes):
+        img = img/255.0
+        width, height = img.shape[0], img.shape[1]
+        if (height != new_size[1]) or (width != new_size[0]):
+            img = tf.image.resize(img, new_size)
+            x1 = boxes[:,0]*(new_size[0]/width)
+            y1 = boxes[:,1]*(new_size[1]/height)
+            x2 = boxes[:,2]*(new_size[0]/width)  
+            y2 = boxes[:,3]*(new_size[1]/height)
+            boxes = tf.stack([x1, y1, x2, y2, boxes[:,4]], axis=1)
+        return img, boxes
 
-    def set_labels(img, label):
-        label_out = mask2categorical(label, labelmap)
-        return img, label_out
-
-    def resize(img, label):
-        X = tf.image.resize(img, shape)
-        Y = tf.image.resize(label, shape, method='nearest' )
-        return X, Y
-
-    pre_ds = ds.map(normalize)
-    pre_ds = pre_ds.map(resize)
-    pre_ds = pre_ds.map(set_labels)
+    pre_ds = ds.map(img_pre)
     return pre_ds
 
-def train_val_split(ds: "tf.data.Dataset", val_size: float = 0.2):
-    len_ds = len(list(ds.as_numpy_iterator()))
-    train_size = int((1 - val_size) * len_ds)
-
-    ds_train = ds.take(train_size)
-    ds_val = ds.skip(train_size)
-    return ds_train, ds_val
+def ds_encode():
+    #TODO
+    pass
 
 if __name__ == "__main__":
-    IMG_PATH = "../data/DatasetE2/JPEGImages"    
-    LABEL_PATH = "../data/DatasetE2/SegmentationClass"
-    LABELMAP = parse_labelfile("../data/DatasetE2/labelmap.txt")
-    IMG_PATHS = np.sort([path.join(IMG_PATH, img) for img in os.listdir(IMG_PATH)])
-    LABEL_PATHS = np.sort([path.join(LABEL_PATH, img) for img in os.listdir(LABEL_PATH)])
-    ds = read_data(IMG_PATHS, LABEL_PATHS)
-    pre_ds = preprocess_ds(ds, LABELMAP, (512, 512))
-    pass
+    IMG_PATH = "../data/GermPredDataset/ZeaMays/img"
+    ANNS_PATH = "../data/GermPredDataset/ZeaMays/true_ann"
+    ds = data_read(IMG_PATH, ANNS_PATH)
+    pre_ds = data_preprocess(ds)
+
+    import matplotlib.pyplot as plt
+    for x, y in pre_ds.take(5):
+        box_xywh = get_xyhw(y)
+        corner = get_corners(box_xywh)
+        plt.imshow(bndboxes_draw(x, y)); plt.show()
